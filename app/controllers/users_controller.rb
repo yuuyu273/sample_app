aa238d34de3dfe4dcf171877dda9e24edf40c8af
yuuyu <99785532+yuuyu273@users.noreply.github.com>
2023-01-10 21:04:04 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
-  def new
+
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+    #debugger
   end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # POST /users (+ )
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      # GET "/users/#{@user.id}"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
